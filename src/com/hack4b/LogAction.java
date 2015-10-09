74a8a0006a04ffa1d0974ea4e91bdc47bd9a1a05
on ice <hitr@foxmail.com>
2015-10-09 18:26:03 +0800

    完成配置文件和日志的操作类

---
+++ src/com/hack4b/LogAction.java
@@ -0,0 +1,39 @@
+package com.hack4b;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.dom4j.DocumentException;
+
+/**
+ * 该类提供日志的写入操作
+ * @author on ice
+ *
+ */
+public class LogAction {
+	
+	File logFile = null;
+	
+	PrintWriter pw = null;
+	
+	public LogAction() throws DocumentException, IOException {
+		ConfReader confReader = new ConfReader();
+		logFile = new File(confReader.getConf().get(1));
+		if(logFile.exists()==false){
+			logFile.createNewFile();
+		}
+	}
+	
+	/**
+	 * 写入日志文件
+	 * @param info
+	 * @throws FileNotFoundException
+	 */
+	public void writeLog(String info) throws FileNotFoundException{
+		pw = new PrintWriter(logFile);
+		pw.write(info);
+		pw.close();
+	}
+}
---
+++ src/com/hack4b/Main.java
@@ -0,0 +1,7 @@
+package com.hack4b;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
