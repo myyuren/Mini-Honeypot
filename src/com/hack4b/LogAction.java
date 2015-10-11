f2e2b64d41338c7f5ad8ad24305492447d31efcf
on ice <hitr@foxmail.com>
2015-10-11 10:58:21 +0800

    修复日志文件写入的一个Bug

--- src/com/hack4b/LogAction.java
+++ src/com/hack4b/LogAction.java
@@ -1,7 +1,7 @@
 package com.hack4b;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -16,27 +16,54 @@ public class LogAction {
 	
 	File logFile = null;
 	
+	FileWriter fw = null;
+	
 	PrintWriter pw = null;
 	
-	public LogAction() throws DocumentException, IOException {
+	/**
+	 * 打开日志文件，并获得输出流
+	 * @throws DocumentException
+	 * @throws IOException
+	 */
+	public void openLogFile(){
 		//创建配置文件阅读器对象
-		ConfReader confReader = new ConfReader();
-		//获取日志文件路径
-		logFile = new File(confReader.getLog());
-		//如果文件不存在，则创建
-		if(logFile.exists()==false){
-			logFile.createNewFile();
+		ConfReader confReader;
+		try {
+			confReader = new ConfReader();
+			//获取日志文件路径
+			logFile = new File(confReader.getLog());
+			//如果文件不存在，则创建
+			if(logFile.exists()==false){
+				logFile.createNewFile();
+			}
+			fw = new FileWriter(logFile,true);
+			pw = new PrintWriter(fw);
+		} catch (DocumentException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 	
 	/**
 	 * 写入日志文件
 	 * @param info
-	 * @throws FileNotFoundException
+	 * @throws IOException 
 	 */
-	public void writeLog(String info) throws FileNotFoundException{
-		pw = new PrintWriter(logFile);
+	public void writeLog(String info) throws IOException{
+		pw = new PrintWriter(fw);
 		pw.println(info);
-		pw.close();
+	}
+	
+	/**
+	 * 清空缓存并关闭输出流
+	 */
+	public void closeLogFile(){
+		try {
+			fw.flush();
+			pw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- src/com/hack4b/RunnablePort.java
+++ src/com/hack4b/RunnablePort.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import org.dom4j.DocumentException;
 
 /**
  * 本类提供端口监听的线程操作
@@ -26,22 +25,27 @@ public class RunnablePort implements Runnable {
 	public void run() {
 		while(true){
 			try {
-				//获取远程客户端
+				//获取远程主机
 				Socket remote = localHost.accept();
-				//获取远程客户端的IP地址
-				String remoteIP = remote.getLocalAddress().getHostAddress();
-				//获取远程客户端的主机名
-				String remoteHostName = remote.getLocalAddress().getHostName();
+				//获取远程主机的IP地址
+				String remoteIP = remote.getInetAddress().getHostAddress();
+				//获取远程主机的主机名
+				String remoteHostName = remote.getInetAddress().getHostName();
+				//获取远程主机的端口号
+				int remotePort = remote.getLocalPort();
+				//获取日志操作对象
+				LogAction logAction = new LogAction();
+				//打开日志文件
+				logAction.openLogFile();
 				//写入日志
-				new LogAction().writeLog("主机名："+remoteHostName+"--IP地址："+remoteIP);
-				//关闭连接
+				logAction.writeLog("主机名："+remoteHostName+"--IP地址："+remoteIP+":"+remotePort);
+				//关闭日志文件
+				logAction.closeLogFile();
+				//关闭远程主机连接
 				remote.close();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (DocumentException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				System.err.println("抱歉，程序出了点问题，需要你重启下 - -!");
+				return;
 			}
 		}
 		
