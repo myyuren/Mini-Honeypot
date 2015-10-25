789a6d2ffc2eee7c8493dc37dd3e43e1790ad020
on ice <hitr@foxmail.com>
2015-10-25 23:12:54 +0800

    优化程序逻辑，更新项目成品

--- src/com/hack4b/Main.java
+++ src/com/hack4b/Main.java
@@ -12,10 +12,10 @@ import org.dom4j.DocumentException;
 public class Main {
 	public static void main(String[] args) {
 		try {
-			//开启端口监听
-			new ListenPortAction().startListen();
 			System.out.println("已经开始工作了……");
 			System.out.println("所有的攻击都会显示在本窗口上并写入日志。  -- on ice");
+			//开启端口监听
+			new ListenPortAction().startListen();
 		} catch (NumberFormatException | DocumentException | IOException e) {
 			e.printStackTrace();
 			System.err.println("抱歉，程序出了点错误，需要你重启下- -!");
