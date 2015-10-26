d253e71cd76dc2f29dd584a2fc4491b7b3afabcd
on ice <hitr@foxmail.com>
2015-10-26 16:28:27 +0800

    修正日志格式

--- src/com/hack4b/RunnablePort.java
+++ src/com/hack4b/RunnablePort.java
@@ -31,24 +31,26 @@ public class RunnablePort implements Runnable {
 	public void run() {
 		while(true){
 			try {
+				//获取日志写入器
+				LogAction logAction = new LogAction();
+				//打开日志文件
+				logAction.openLogFile();
 				//等待远程主机连接
 				Socket remote = localHost.accept();
 				//获取远程主机的IP地址
 				String remoteIP = remote.getInetAddress().getHostAddress();
 				//获取远程主机的主机名
 				String remoteHostName = remote.getInetAddress().getHostName();
-				//获取远程主机的端口号
-				int remotePort = remote.getPort();
-				//获取被访问端口号
+				//获取主机名失败
+				if(remoteIP.equals(remoteHostName)){
+					remoteHostName = "主机名未知";
+				}
+				//获取被攻击端口号
 				int localPort = remote.getLocalPort();
 				//获取时间
-				String date = new SimpleDateFormat("yyyy年MM月dd日-->HH:mm:ss").format(new Date());
-				//获取日志写入器
-				LogAction logAction = new LogAction();
-				//打开日志文件
-				logAction.openLogFile();
+				String date = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss").format(new Date());
 				//写入日志
-				String logLine = "远程主机名："+remoteHostName+"-->IP地址："+remoteIP+":"+remotePort+"-->被访问端口："+localPort+"-->"+date;
+				String logLine = "发现目标："+remoteHostName+" IP地址："+remoteIP+" 攻击端口："+localPort+" 时间："+date;
 				logAction.writeLog(logLine);
 				//关闭日志文件
 				logAction.closeLogFile();
@@ -60,14 +62,18 @@ public class RunnablePort implements Runnable {
 				ConfReader cfgReader = new ConfReader();
 				//获取配置文件消息
 				String message = cfgReader.getMessage();
-				//发送消息给远程主机
-				pw.write(message);
+				//如果消息不为空，发送消息给远程主机
+				if(!"".equals(message)&&message!=null){
+					pw.write(message);
+				}
 				//刷新缓冲区
 				pw.flush();
 				//关闭输出流
 				pw.close();
 				//关闭远程主机连接
-				remote.close();
+				if(remote.isConnected()){
+					remote.close();
+				}
 			} catch (IOException e) {
 				System.err.println("抱歉，程序出了点问题，需要你重启下。");
 				return;
